dec0fb66d7eb60bef3e60af2d66e0c694b29d8fe
Jon Evans <jon@craftyjon.com>
2014-11-30 20:55:42 -0500

    Port: Wake on plug (1/2)
    abc: set off by default, we have battery led
    use an user system setting instead of global
    
    Change-Id: I5e50a7fd351857d76c1c2fd13c6e737d8902a2f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4008,6 +4008,13 @@ public final class Settings {
          */
         public static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
 
+       /**
+         * Whether to wake the display when plugging or unplugging the charger
+         *
+         * @hide
+         */
+        public static final String WAKE_WHEN_PLUGGED_OR_UNPLUGGED = "wake_when_plugged_or_unplugged";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
