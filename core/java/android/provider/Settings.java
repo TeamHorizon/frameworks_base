68540eed2a602a1e0080e9bba8acff48b724d864
ezio84 <brabus84@gmail.com>
2016-10-11 16:33:17 +0200

    Add NavBar toggle [1/2]
    todo: createNavigationBar try-catch gives protection against
    a monkey toggling the navbar switch more times and quickly
    (NavigationBarFragment create() could try to add the view
    navigationBarView while the old one still exists) but it's not
    the best solution, it'd better to check if the activity isFinishing
    or something similar
    
    Change-Id: I0050e8e91b48ebd3ff1c313a0830f3ec17f94ad8

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7132,6 +7132,12 @@ public final class Settings {
         public static final String QS_AUTO_ADDED_TILES = "qs_auto_tiles";
 
         /**
+         * Whether to enable navigation bar
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_ENABLED = "navigation_bar_enabled";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
