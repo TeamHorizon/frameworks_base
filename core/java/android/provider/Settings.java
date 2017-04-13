9d53658da4e1bab1e021b15e1ecedb0e0e22d763
Varun Date <date.varun123@gmail.com>
2017-04-13 22:52:39 +0530

    Option to Display Data Disabled Indicator Icon [1/2]
    Change-Id: I7e6f1ec75d589dd7c7a5886c936b95d56b30facb
    
    * don't use tuner
    
    Signed-off-by: dadi11 <dadi.sikorski@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4249,6 +4249,13 @@ public final class Settings {
         public static final String QS_QUICKBAR_SCROLL_ENABLED = "qs_quickbar_scroll_enabled";
 
         /**
+         *
+         * Whether to display cross sign for a data disabled connection
+         * @hide
+         */
+        public static final String DATA_DISABLED_ICON = "data_disabled_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
