1e41d178eaf1c697a59759732d983650549abd81
cristianomatos <cristianobmatos@gmail.com>
2017-09-05 13:08:19 +0800

    Recents Clear All Button Tweaks and Add FAB to Recents Panel
    Thanks to RR and @varund7726
    
    Extracted, cleaned and updated for Nitrogen OS by @xyyx
    
    Change-Id: Id986400978e78418642a8826512f6c3af876bda5

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3930,6 +3930,18 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_LOCKSCREEN =
                 "double_tap_sleep_lockscreen";
 
+        /**
+         * show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
         /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
          *
          * @hide
