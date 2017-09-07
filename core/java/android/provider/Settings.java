0f63f14f68f86bc3b4d7b9c891b97a9591120ae0
xyyx <xyyx@mail.ru>
2017-09-07 11:16:04 +0800

    Ability to disable dashboards conditions (1/2)
    Change-Id: I09cc425745f1e37425639660e9de336e6a56e03c

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3936,6 +3936,12 @@ public final class Settings {
          */
 
         /**
+         * Disable dashboard conditions in settings
+         * @hide
+         */
+        public static final String ENABLE_CONDITIONS = "enable_conditions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
