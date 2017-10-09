0ae5dab3ee4c4c38d884a721eebaae99ea1071f4
Dave Kessler <activethrasher00@gmail.com>
2017-10-09 22:21:32 +0200

    fwb: Add membar to recents options [1/2]
    Change-Id: Ieaf86a2360cc993bb20f971b25c2b8c49a746f64

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4115,6 +4115,12 @@ public final class Settings {
         public static final String FORCE_AMBIENT_FOR_MEDIA = "force_ambient_for_media";
 
         /**
+          * show the membar in recents
+          *  @hide
+          */
+        public static final String SYSTEMUI_RECENTS_MEM_DISPLAY = "systemui_recents_mem_display";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
