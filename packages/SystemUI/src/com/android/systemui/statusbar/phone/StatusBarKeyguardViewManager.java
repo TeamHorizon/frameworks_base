AOSPA/android_frameworks_base-old

Fingerprint: Speed up wake-and-unlock scenario
* Directly dismiss keyguard instead of using a fade-out animation
* Skip virtual navigation button animation on wake

This patch is for Nougat.

Change-Id: I7d479ce76526e0051b838053fdeba99dd5c86dd2
Signed-off-by: Alex Naidis <alex.naidis@linux.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -370,6 +370,20 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         mBouncer.hide(true /* destroyView */);
     }
 
+    public void hideNoAnimation() {
+        mShowing = false;
+        mPhoneStatusBar.setKeyguardFadingAway(SystemClock.uptimeMillis(), 0, 0);
+        mPhoneStatusBar.hideKeyguard();
+        mPhoneStatusBar.finishKeyguardFadingAway();
+        mStatusBarWindowManager.setKeyguardShowing(false);
+        mBouncer.hide(true /* destroyView */);
+        mViewMediatorCallback.keyguardGone();
+        mFingerprintUnlockController.finishKeyguardFadingAway();
+        updateStates();
+        WindowManagerGlobal.getInstance().trimMemory(
+                ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN);
+    }
+
     private void animateScrimControllerKeyguardFadingOut(long delay, long duration,
             boolean skipFirstFrame) {
         animateScrimControllerKeyguardFadingOut(delay, duration, null /* endRunnable */,

TeamHorizon/frameworks_base

 * Directly dismiss keyguard instead of using a fade-out animation
 * Skip virtual navigation button animation on wake

packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarKeyguardViewManager.java
@@ -478,6 +478,20 @@ public class StatusBarKeyguardViewManager implements RemoteInputController.Callb
         mBouncer.prepare();
     }
 
+    public void hideNoAnimation() {
+        mShowing = false;
+        mStatusBar.setKeyguardFadingAway(SystemClock.uptimeMillis(), 0, 0);
+        mStatusBar.hideKeyguard();
+        mStatusBar.finishKeyguardFadingAway();
+        mStatusBarWindowManager.setKeyguardShowing(false);
+        mBouncer.hide(true /* destroyView */);
+        mViewMediatorCallback.keyguardGone();
+        mFingerprintUnlockController.finishKeyguardFadingAway();
+        updateStates();
+        WindowManagerGlobal.getInstance().trimMemory(
+                ComponentCallbacks2.TRIM_MEMORY_UI_HIDDEN);
+    }
+
     private void animateScrimControllerKeyguardFadingOut(long delay, long duration,
             boolean skipFirstFrame) {
         animateScrimControllerKeyguardFadingOut(delay, duration, null /* endRunnable */,