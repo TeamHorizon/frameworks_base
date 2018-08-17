32db162c1275c5b8f8d846aef4f9dfe2ea253d81
maxwen <max.weninger@gmail.com>
2018-08-17 16:04:35 +0200

    base: SystemUI: enable tuner and show in OmniGears
    Change-Id: I4d3ff3ca989e6a6952419d147208318ee269b9d7
    Signed-off-by: xyyx <xyyx@mail.ru>

--- packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
+++ packages/SystemUI/src/com/android/systemui/tuner/TunerServiceImpl.java
@@ -93,6 +93,7 @@ public class TunerServiceImpl extends TunerService {
             }
         };
         mUserTracker.startTracking();
+        setTunerEnabled(mContext, true);
     }
 
     @Override
