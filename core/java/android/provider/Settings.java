dbc1e819a8f703771f8bf0fef04c9a555dd6982a
dadi11 <dadi.sikorski@gmail.com>
2019-06-01 22:19:47 +0200

    Revert "base: Add linked ringtone and notification volumes"
    This reverts commit 3519be3c37e139e2779499d80d168ec0fe4ad097.
    
    Change-Id: If9bf939a98617566745e8fc79d2820ddff132e71

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8314,15 +8314,6 @@ public final class Settings {
         public static final String FORCE_AUTHORIZE_SUBSTRATUM_PACKAGES = "force_authorize_substratum_packages";
 
         /**
-        * Boolean value whether to link ringtone and notification volume
-        * @hide
-        */
-        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
-        /** @hide */
-        private static final Validator VOLUME_LINK_NOTIFICATION_VALIDATOR =
-               BOOLEAN_VALIDATOR;
-
-        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -8420,7 +8411,6 @@ public final class Settings {
             SCREENSAVER_ACTIVATE_ON_SLEEP,
             LOCKDOWN_IN_POWER_MENU,
             SHOW_FIRST_CRASH_DIALOG_DEV_OPTION,
-            VOLUME_LINK_NOTIFICATION,
             VOLUME_HUSH_GESTURE,
             MANUAL_RINGER_TOGGLE_COUNT,
             HUSH_GESTURE_USED,
@@ -8570,7 +8560,6 @@ public final class Settings {
                     ENABLED_NOTIFICATION_POLICY_ACCESS_PACKAGES_VALIDATOR); //legacy restore setting
             VALIDATORS.put(HUSH_GESTURE_USED, HUSH_GESTURE_USED_VALIDATOR);
             VALIDATORS.put(MANUAL_RINGER_TOGGLE_COUNT, MANUAL_RINGER_TOGGLE_COUNT_VALIDATOR);
-            VALIDATORS.put(VOLUME_LINK_NOTIFICATION, VOLUME_LINK_NOTIFICATION_VALIDATOR);
             VALIDATORS.put(LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, BOOLEAN_VALIDATOR);
             VALIDATORS.put(LOCK_SCREEN_SHOW_NOTIFICATIONS, BOOLEAN_VALIDATOR);
         }
